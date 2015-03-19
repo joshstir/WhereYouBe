8be0f3746f83a91bb6ea15633219d5115fa105aa
joshstir <joshostir@gmail.com>
2015-03-18 22:37:44 -0400

    add icons

--- app/src/main/java/com/jos/sss/whereyoube/MainWhereYouBeActivity.java
+++ app/src/main/java/com/jos/sss/whereyoube/MainWhereYouBeActivity.java
@@ -1,7 +1,10 @@
 package com.jos.sss.whereyoube;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ParseException;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
@@ -12,8 +15,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
+
+import java.util.List;
 
 /**
  * Created by joshs on 3/15/2015.
@@ -56,11 +63,44 @@ public class MainWhereYouBeActivity extends ActionBarActivity implements SearchV
         inflater.inflate(R.menu.mainmenu, menu);
         this.menu = menu;
         MenuItem item = menu.findItem(R.id.action_search);
+
+        SearchManager manager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
         if (item != null){
             searchView = (SearchView)item.getActionView();
+            searchView.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    loadData(query);
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    return false;
+                }
+            });
         }
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void loadData(String querytext) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("name", querytext);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, com.parse.ParseException e) {
+                if (e == null){
+                    //query ok
+                    Log.d("QUERY",parseUsers.get(0).getEmail());
+                }
+                else{
+                    //something was wrong
+                    Log.d("QUERY", e.getMessage());
+                }
+            }
+        });
     }
 
     @Override
